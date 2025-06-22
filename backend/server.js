907945cdb8cdd909cbf9f0f43d05e26e0bde8dc3
fulbrecht <13141050+fulbrecht@users.noreply.github.com>
2025-06-22 01:34:05 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const authRoutes = require('./routes/auth');
 const taskRoutes = require('./routes/tasks');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // --- Middleware ---
 app.use(express.json());
